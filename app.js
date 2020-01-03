ab74bda25ed8df0793a28ae7c162fa764b92fda0
Muhammad Rendra <muhammadnurrendra@gmail.com>
2020-01-04 01:50:55 +0700

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const helmet = require('helmet')
 const compression = require('compression')
+const cors = require('cors')
 // require local modules
 const routes = require('./routes')
 const { invalidJsonHandler } = require('./errors')
@@ -12,6 +13,7 @@ const app = express()
 // middleware
 app.use(helmet())
 app.use(compression())
+app.use(cors())
 app.use(express.json())
 app.use(invalidJsonHandler)
 app.use(routes)
